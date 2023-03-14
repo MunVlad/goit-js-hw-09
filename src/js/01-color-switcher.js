58f5103840945ca374278068ada09c5d6e84d599
MunVlad <mvv2006@ukr.net>
2023-03-14 19:17:46 +0200

    1-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
 
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+    let intervalId;
+
+    function startBackgroundChanging() {
+      startButton.disabled = true; // отключаем кнопку Start
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    }
+
+    function stopBackgroundChanging() {
+      startButton.disabled = false; // включаем кнопку Start обратно
+      clearInterval(intervalId);
+    }
+
+    startButton.addEventListener('click', startBackgroundChanging);
+    stopButton.addEventListener('click', stopBackgroundChanging);
